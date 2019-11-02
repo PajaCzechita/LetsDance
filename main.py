48cda07e16edd344f70713933f4062ab4d86acce
Barbora Hájková <hajkbar@gmail.com>
2019-11-02 17:41:54 +0100

    registracni formular pro kurzy
    vysledek smazeni na hackatonu

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-from db_functions import get_lectureType
+from flask import Flask, render_template, redirect, url_for
+import db_functions
+from forms.registration import RegistrationForm
 application = Flask('letsdance') 
+
+
 
 @application.route('/')
 def show_layout():
@@ -9,4 +12,27 @@ def show_layout():
 
 @application.route('/formular')
 def show_formular():
-    return render_template('formular.html', type=get_lectureType())
+    return render_template('formular.html', type=db_functions.get_lectureType())
+
+
+@application.route('/registrace', methods = ['POST', 'GET'])
+def add_course():
+    form = RegistrationForm(csrf_enabled = False)
+    if form.validate_on_submit(): 
+        db_functions.insert_classes(
+          form.lesson.data, 
+          form.lecturer.data, 
+          form.studio.data, 
+          form.day.data, 
+          form.time_from.data,
+          form.time_to,
+          form.age_group.data,
+          form.level.data,
+          form.course_type.data,
+          form.address.data,
+          form.address_city.data,
+          form.link.data,
+          form.email_address.data,
+          )
+        return redirect (url_for ("show_formular"))
+    return render_template('registrace.html', form = form)
