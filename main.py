e3c7bc35c008d473f2d5c3c519adab42169504e4
Paja Bajgarova <p.bajgarova@gmail.com>
2019-11-02 12:28:28 +0100

    hackathon1

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from db_functions import get_lectureType
 application = Flask('letsdance') 
 
 @application.route('/')
-def show_index():
-    return render_template('index.html')
+def show_layout():
+    return render_template('layout.html')
     
+
+@application.route('/formular')
+def show_formular():
+    return render_template('formular.html', type=get_lectureType())
